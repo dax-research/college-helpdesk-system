f09ad26f8ab8d37ba1563857d7bef9087f177b66
dax-research <dax1214b@gmail.com>
2026-03-22 21:31:51 +0530

    initial project setup

---
+++ src/main/java/com/ddu/college_helpdesk_system/CollegeHelpdeskSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ddu.college_helpdesk_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeHelpdeskSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeHelpdeskSystemApplication.class, args);
+	}
+
+}
